7f29817a2eb61d5fb2e01b8516ec6a6ca14046e0
k111g <P.Subedi3928@student.leedsbeckett.ac.uk>
2020-05-28 17:46:41 +0545

    Added login form

--- index.php
+++ index.php
@@ -1,3 +1,53 @@
-<?php
-echo "Hello!!"
-?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Sign up or Sign in</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="uk-section uk-container">
+  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+			<form class="uk-form-stacked js-login">
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email/ईमेल</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password/पासवर्ड</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <button class="uk-button uk-button-default" type="submit">Login</button>
+			    </div>
+
+			</form>
+  		</div>
+  	    </div>
+
+        
+        <!-- jQuery is required -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" async defer></script>
+
+        <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+    </body>
+</html>
