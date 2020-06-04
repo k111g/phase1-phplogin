3e9ef5b1ba4aebfb9228719ab1bfaf90e4e57e51
k111g <P.Subedi3928@student.leedsbeckett.ac.uk>
2020-06-04 14:03:02 +0545

    More code added

--- login.php
+++ login.php
@@ -11,28 +11,46 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>INDEX.php</title>
+        <title>Sign in</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css">
     </head>
     <body>
+    <p><a href="http://localhost/phase1-3html-2web-LoginRegistrationProject/phase1-phplogin/index.php">&larr; Back to Index.php!</p>
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
         <div class="uk-section uk-container">
-			<?php
-			    echo "Greetings Earthling!<br />Today is: ";
-			    echo date("Y m d");
-			    echo "<br />";
-			    echo "Login System Project. Learning PHP!"
-			?>
-		    <p>
-		        <a href="http://localhost/phase1-3html-2web-LoginRegistrationProject/phase1-phplogin/login.php">Login</a>
-		        <a href="http://localhost/phase1-3html-2web-LoginRegistrationProject/phase1-phplogin/register.php">Register</a>
-		    </p>
+
+  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+			<form class="uk-form-stacked js-login">
+			<h2>LOGIN</h2>
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email/ईमेल</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password/पासवर्ड</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <button class="uk-button uk-button-default" type="submit">Login</button>
+			    </div>
+
+			</form>
+  		</div>
   	    </div>
+
         <?php require_once "includes/footer.php"; ?>
+        
     </body>
 </html>
